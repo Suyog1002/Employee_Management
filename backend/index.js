e16955424cf6c8ed1dddba4879d5a2a2275fd745
Suyog1002 <141540723+Suyog1002@users.noreply.github.com>
2023-12-16 21:22:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ import router from './routes/route.js';
 
 const app=express(); //express server created
 const port=8000;
-app.use(cors());
+app.use(cors({
+    origin: 'https://employee-management-wine-five.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 
